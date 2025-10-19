c5fdd2a1bb31fbd816e00825b3df187be9ae11ef
devovi2024 <arfanhosenovi@gmail.com>
2025-10-19 16:24:01 +0600

    add CRUD operations for tasks with status-based listing

---
+++ src/controllers/TasksController.js
@@ -0,0 +1,62 @@
+exports.createTask = (req, res) => {
+    let reqBody = req.body;
+    reqBody.email = req.headers['email'];
+
+    TasksModel.create(reqBody, (err, data) => {
+        if(err){
+            console.log(err);
+            return res.status(500).json({ message: "Task creation failed", error: err });
+        } 
+        res.status(201).json({ message: "Task created successfully", task: data });
+    });
+}
+
+exports.updateTask = (req, res) => {
+    let id = req.params.id;
+    let status = req.params.status;
+    let Query = { _id: id };
+    let reqBody = { status: status };
+
+    TasksModel.updateOne(Query, reqBody, (err, data) => {
+        if(err){
+            return res.status(500).json({ message: "Task update failed", error: err });
+        }
+
+        res.status(200).json({ message: "Task updated successfully", result: data });
+    });
+}
+
+
+exports.deleteTask = (req, res) => {
+    let id = req.params.id;
+    TasksModel.deleteOne({ _id: id }, (err, data) => {
+        if(err){
+            return res.status(500).json({ message: "Task deletion failed", error: err });
+        }
+        res.status(200).json({ message: "Task deleted successfully", result: data });
+    });
+}
+
+
+exports.listTaskByStatus = (req, res) => {
+    let status = req.params.status;
+    let email = req.headers['email'];
+
+    TasksModel.aggregate([
+        { $match: { status: status, email: email } },
+        { $project: {
+            _id: 1,
+            title: 1,
+            description: 1,
+            status: 1,
+            createdDate: {
+                $dateToString: { format: "%d-%m-%Y", date: "$createdDate" }
+            }
+        }}
+    ], (err, data) => {
+        if(err){
+            return res.status(500).json({ message: "Task fetch failed", error: err });
+        }
+        res.status(200).json({ message: "Tasks fetched successfully", tasks: data });
+    });
+}
